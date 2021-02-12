0b8e4c467222f85a5ac31665f6bd12771d82d41c
Luke-10100 <77681899+Luke-10100@users.noreply.github.com>
2021-02-12 13:44:38 +0000

    Created saving routine using the file select registers.

--- main.s
+++ main.s
@@ -34,20 +34,43 @@ start:
 	movwf	TRISC, A	    ; Port C all outputs
 	movlw	0xff
 	movwf	TRISD, A	    ; Port d all input
+	counter equ 0x06
+ 	movlw	0x0
+	movwf	0x06, A
+	lfsr	0, 0x140
+	lfsr	1, 0x140
+	bra 	write_test
+write_loop:
+;	call	delay
+	movf	0x06, w, a
+	movwf	POSTINC0, f, a
+	incf 	0x06, W, A
+write_test:
+	movwf	0x06, A	    ; Test for end of loop condition
+	movlw 	0x1A	    ; Count up to this number OR
+;	movf	PORTD, W    ; Count up to input number on port D
+	cpfsgt 	0x06, A
+	bra 	write_loop		    ; Not yet finished goto start of loop again
 	movlw	0x0
 	movwf	0x06, A
-	bra 	test
-loop:
-	call	delay
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-;	movlw 	0x63	    ; Count up to this number OR
-	movf	PORTD, W    ; Count up to input number on port D
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+	lfsr	1, 0x140
+	goto read_test
+;	goto 	0x0		    ; Re-run program from start
 	;test
-
+read_loop: 
+	call	delay
+	movf	POSTINC1, w, a
+	movwf	PORTC, f, a
+	incf 	0x06, W, A
+	
+read_test:
+	movwf	0x06, A	    ; Test for end of loop condition
+	movlw 	0x1A	    ; Count up to this number OR
+	cpfsgt 	0x06, A
+	bra 	read_loop		    ; Not yet finished goto start of loop again
+	movlw	0x0
+	movwf	0x06, A
+	lfsr	1, 0x140
+	goto read_loop
+	
 	end	main
