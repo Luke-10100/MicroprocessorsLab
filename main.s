2c8f852ed66a37516447776fcd3eb6fb3a8e57e1
Luke-10100 <77681899+Luke-10100@users.noreply.github.com>
2021-02-16 16:12:36 +0000

    Clock pulse and read and write subroutines.

--- main.s
+++ main.s
@@ -16,29 +16,65 @@ delay:
 	bra delay
 	return
 delay1: 
-	movlw	0x0f
+	movlw	0xff
 	movwf	0x40, A
 	call delay2
 	decfsz 0x30, A
 	bra delay1
 	return
 delay2: 
-	decfsz 0x40, A
+	decfsz 0xff, A
 	bra delay2
 	return
 setup: 
 	movlw 	0x0
 	movwf	TRISD, A	    ; Port C all outputs
-	movlw	0xff
-;	movlw	0x1
-;	movwf	RD0
 	bsf	PORTD, 0, A
 	bsf	PORTD, 1, A
-	bcf	PORTD, 2, A
+	bsf	PORTD, 2, A
 	bsf	PORTD, 3, A
+	return
+clockPulse1: 
+	bcf	PORTD, 0, A
+	bsf	PORTD, 0, A
+	return
+clockPulse2: 
+	bcf	PORTD, 1, A
+	bsf	PORTD, 1, A
+	return
+readMem1:
+	bsf	PORTD, 3, A		; OE2 high
+	bcf	PORTD, 2, A		; OE1 low
+	call	clockPulse1		; c1 pulse
+	bsf	PORTD, 2, A		; Reset OE1
+					; c2 nothing
+	return
+readMem2: 
+    	bsf	PORTD, 2, A		; OE1 high
+	bcf	PORTD, 3, A		; OE2 low
+	call	clockPulse2		; c2 pulse
+	bsf	PORTD, 3, A		; Reset OE2
+					; c1 nothing
+	return
+writeMem1: 
+	bsf	PORTD, 2, A		; OE1 high
+	bcf	PORTD, 3, A		; OE2 low
+	call	clockPulse1		; c1 pulse
+	bsf	PORTD, 3, A		; Reset OE2
+					; c2 nothing
+	return
+writeMem2: 
+	bsf	PORTD, 3, A		; OE2 high
+	bcf	PORTD, 2, A		; OE1 low
+	call	clockPulse2		; c2 pulse
+	bsf	PORTD, 2, A		; Reset OE1
+					; c1 nothing
 	return
 start:
 	call setup
-	
+	call readMem1
+	call writeMem1
+	call readMem2
+	call writeMem2
 	goto start
 	end	main
